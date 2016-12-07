76e0efd3ece1715bdd95191d2baf4299db64195f
Romain Brunie <brunie.romain@outlook.fr>
2016-12-07 23:58:32 +0100

    Initial Commit

---
+++ NewSpotifyApp/NewSpotifyApp-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  NewSpotifyApp-Bridging-Header.h
+//  NewSpotifyApp
+//
+//  Created by Romain Brunie on 12/7/16.
+//  Copyright © 2016 Romain Brunie. All rights reserved.
+//
+
+#ifndef NewSpotifyApp_Bridging_Header_h
+#define NewSpotifyApp_Bridging_Header_h
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+
+#endif /* NewSpotifyApp_Bridging_Header_h */
