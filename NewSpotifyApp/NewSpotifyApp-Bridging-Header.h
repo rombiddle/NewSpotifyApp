934cc555b067a4f232219575392f8ff47bb71de3
Romain Brunie <brunie.romain@outlook.fr>
2016-12-08 14:11:42 +0100

    spotify framework setup

--- NewSpotifyApp/NewSpotifyApp-Bridging-Header.h
+++ NewSpotifyApp/NewSpotifyApp-Bridging-Header.h
@@ -1,16 +1,7 @@
 //
-//  NewSpotifyApp-Bridging-Header.h
-//  NewSpotifyApp
+//  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-//  Created by Romain Brunie on 12/7/16.
-//  Copyright © 2016 Romain Brunie. All rights reserved.
-//
-
-#ifndef NewSpotifyApp_Bridging_Header_h
-#define NewSpotifyApp_Bridging_Header_h
 
 #import <SpotifyAuthentication/SpotifyAuthentication.h>
 #import <SpotifyMetadata/SpotifyMetadata.h>
 #import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
-
-#endif /* NewSpotifyApp_Bridging_Header_h */
---
+++ NewSpotifyApp/NewSpotifyApp-Bridging-Header.m
@@ -0,0 +1,13 @@
+//
+//  NewSpotifyApp-Bridging-Header.m
+//  NewSpotifyApp
+//
+//  Created by Romain Brunie on 12/8/16.
+//  Copyright © 2016 Romain Brunie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
